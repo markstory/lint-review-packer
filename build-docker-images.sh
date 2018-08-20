05acee9829f02a5fc3272fad26d750eb24aa2902
Mark Story <mark@mark-story.com>
2018-08-19 22:51:20 -0400

    Add docker image buildout.

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+# Install and enable docker.
+DEBIAN_FRONTEND=noninteractive apt-get install -y docker.io
+systemctl start docker
+systemctl enable docker
+
+# Enable ubuntu user to use docker
+usermod -aG docker ubuntu
+
 # Get lintreview
 git clone https://github.com/markstory/lint-review /tmp/lint-review
 
 # Build docker images.
 cd /tmp/lint-review && make images
+rm -rf /tmp/lint-review
